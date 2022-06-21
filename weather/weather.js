9ed6c32356a25af136278d93235d0766f803d532
nitron532 <nitroncreeper200@gmail.com>
2022-06-20 23:53:28 -0700

    fix api

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,6 @@
 
 async function retrieve () {
-    var url = `http://api.weatherapi.com/v1/forecast.json?key=7bed2218a1ac4b92ba5171814221705&q=${sessionStorage.getItem("locationInput")}&days=2&aqi=yes&alerts=no`
+    var url = `https://api.weatherapi.com/v1/forecast.json?key=7bed2218a1ac4b92ba5171814221705&q=${sessionStorage.getItem("locationInput")}&days=2&aqi=yes&alerts=no`
     var req = new Request(url);
     
     const response = await fetch(req);
