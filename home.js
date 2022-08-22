bc4c1978b03cdd1ee836a25f49c23f2f0479a313
nitron532 <nitroncreeper200@gmail.com>
2022-08-22 12:02:12 -0700

    changed some descriptions
    added functionality to about me
    updated some descriptions

--- home.js
+++ home.js
@@ -11,17 +11,26 @@ const main = () =>{
 $(".projects").on("click", () =>{
   $(".homePage").fadeOut();
   $(".skillsPage").fadeOut();
+  $(".aboutMePage").fadeOut();
   $(".projectsPage").delay(500).fadeIn();
 })
 $(".skills").on("click", () =>{
   $(".homePage").fadeOut();
   $(".projectsPage").fadeOut();
+  $(".aboutMePage").fadeOut();
   $(".skillsPage").delay(500).fadeIn();
 })
 $(".backHome").on("click",()=>{
   $(".projectsPage").fadeOut();
   $(".skillsPage").fadeOut();
+  $(".aboutMePage").fadeOut();
   $(".homePage").delay(500).fadeIn();
+})
+$(".aboutMe").on("click", () =>{
+  $(".homePage").fadeOut();
+  $(".skillsPage").fadeOut();
+  $(".projectsPage").fadeOut();
+  $(".aboutMePage").delay(500).fadeIn();
 })
 $("#weather").on("click",() =>{
   window.location.href='weather/input.html';
