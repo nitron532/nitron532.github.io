5fe05f574fdd2638c98540a327a4d318ea77ca18
nitron532 <89716148+nitron532@users.noreply.github.com>
2024-11-26 00:01:03 -0800

    navbar fade out to home

--- stills.js
+++ stills.js
@@ -62,40 +62,7 @@ const main = ()=>{
                 slidesToScroll: 1
               }
             }
-            // {
-            //   breakpoint: 480,
-            //   settings: {
-            //     slidesToShow: 1,
-            //     slidesToScroll: 1
-            //   }
-            // }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
-
-    $(".backHome").on("click",()=>{
-        $("#stillsPage").fadeOut(function(){
-          window.location.href = "index.html";
-        })
-        })
-        //loop thru directory, use length instead of hardcoded counter?
-        /*
-        let stillsList = ["IMG_0399.JPG", "IMG_0395.JPG" ];
-        let counter = 0;
-        let pic = "portfolio\\Stills\\IMG_0399.JPG"; //cover image
-        $("#stillsImage").attr("src",pic);
-        $("#stillsImage").on("click",()=>{
-          if(counter === stillsList.length-1){
-            counter = 0;
-          }
-          else{
-            counter++;
-          }
-          pic = "portfolio\\Stills\\" + stillsList[counter]
-          $("#stillsImage").attr("src",pic);
-        })
-        */
 }
 $(document).ready(main);
