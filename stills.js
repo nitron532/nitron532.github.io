8ef7732141580ea26bce4606d759d57a1ce531c6
nitron532 <89716148+nitron532@users.noreply.github.com>
2024-11-26 00:04:05 -0800

    navbar fade out part two

--- stills.js
+++ stills.js
@@ -1,7 +1,11 @@
 
 const main = ()=>{
     $("#stillsPage").fadeIn();
-
+    $(".backHome").on("click",()=>{
+      $("#stillsPage").fadeOut(function(){
+        window.location.href = "index.html";
+      })
+    })
     $( ".imgExpand" ).hover(
       function() {
         $( this ).addClass("imgTransition");
